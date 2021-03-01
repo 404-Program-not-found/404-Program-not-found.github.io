6491850711d4263a8180f385c68b22f9b805b688
Alex <alex.tyshang@gmail.com>
2021-03-01 20:28:53 +1300

    Update flowchart_handling.js

--- flowchart_handling.js
+++ flowchart_handling.js
@@ -3,6 +3,17 @@ const table = document.getElementById('recommends');
 
 console.log(table);
 
+function reqListener () {
+    var obj = JSON.parse(this.responseText);
+    document.getElementById("mainTitle").textContent = obj["Nodes"]["1"]["Title"]
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", "flowchart_short.json");
+oReq.send();
+
+
 function createCard(title, text_name, img_src) {
     const card = document.createElement('div');
     card.className = "cell";
@@ -22,11 +33,8 @@ function createCard(title, text_name, img_src) {
     return card
 }
 
-var jsonFile;
-fetch("flowchart_short.json")
-    .then(res => res.json())
-    .then(data => jsonFile = JSON.parse(data));
-document.getElementById("mainTitle").textContent = data[Nodes]["1"]["Title"]
+
+
 /* to be replaced with .json or .xml or whatever. I just need it to work*/
 table.appendChild(createCard("Popular Workplace Comedy askhdklahs fjdaklfjlska","Devil is a part timer", "https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABSDLgE7v9x-3dywjUbjUTu6W4dZi3DC0ypUzUGdXujh29uG8tSSKhUpHaRHdHMVNJKhcgsjsFZfSMjEMdx7XxdaWhxgi.jpg?r=072"))
 table.appendChild(createCard("Popular Workplace Comedy","Devil is a part timer", "https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABSDLgE7v9x-3dywjUbjUTu6W4dZi3DC0ypUzUGdXujh29uG8tSSKhUpHaRHdHMVNJKhcgsjsFZfSMjEMdx7XxdaWhxgi.jpg?r=072"))
